108407d954e4e8fd4d4c5f951e1ee44e1b3b663a
0xchabb <bilalchabbi@hotmail.com>
2023-02-15 21:52:28 +0100

    add route table tf and import tf states

--- firewall.tf
+++ firewall.tf
@@ -13,3 +13,18 @@ resource "azurerm_firewall" "example" {
     subnet_id            = "/subscriptions/8fdfcd42-cb6a-4f09-bd1d-984a332c84b1/resourceGroups/myResourceGroup-chabbi/providers/Microsoft.Network/virtualNetworks/myVNet-chabbi/subnets/AzureFirewallSubnet"
   }
 }
+
+
+resource "azurerm_route_table" "example" {
+  name                          = "route-chabbi"
+  location                      = "westeurope"
+  resource_group_name           = "myResourceGroup-chabbi"
+  disable_bgp_route_propagation = false
+
+  route {
+    name           = "chabbi-route"
+    address_prefix = "0.0.0.0/0"
+    next_hop_in_ip_address = "10.21.3.4"
+    next_hop_type  = "VirtualAppliance"
+  }
+}
